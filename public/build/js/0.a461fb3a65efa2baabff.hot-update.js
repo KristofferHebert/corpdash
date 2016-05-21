b29ca1162937164bbbf7c60ab569b0b1da4cc2d3
= <kristoffer@kristofferhebert.com>
2016-05-21 12:23:34 -0700

    refactor: removing hot module json js

--- public/build/js/0.a461fb3a65efa2baabff.hot-update.js
+++
@@ -1,1 +0,0 @@
-webpackHotUpdate(0,{3:function(e,t,r){"use strict";function u(e){return e&&e.__esModule?e:{"default":e}}Object.defineProperty(t,"__esModule",{value:!0});var a=r(1),o=u(a),d=r(23),l=r(39),n=u(l),s=o["default"].createClass({displayName:"App",render:function(){return o["default"].createElement(d.Router,{route:n["default"],history:d.browserHistory})}});t["default"]=s}});
