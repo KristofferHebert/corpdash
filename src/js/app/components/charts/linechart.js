3706b5efb73f5fb8e6d12a712890475fa1cdd3cb
= <kristoffer@kristofferhebert.com>
2016-06-24 20:36:30 -0700

    refactor: adding key metrics tweaks and fixes

--- src/js/app/components/charts/linechart.js
+++ src/js/app/components/charts/linechart.js
@@ -1,37 +1,37 @@
 import React from 'react'
 import rd3 from 'rd3'
+import makeRequest from '../utils/makeRequest'
 
 const LnChrt = rd3.LineChart
-var lineData = [
-  {
-    name: 'Customers over Time',
-    values: [
-    { x: 1436959681000, y: 11 },
-    { x: 1442122919000, y: 9 },
-    { x: 1450703316000, y: 16 },
-    { x: 1459282109000, y: 13 },
-    { x: 1463627239000, y: 10 },
-    { x: 1473627239000, y: 14 },
-    { x: 1483627239000, y: 20 },
-    { x: 1493627239000, y: 14 },
-    { x: 1503627239000, y: 23 },
-    { x: 1513627239000, y: 16 }
-
-    ],
-    strokeWidth: 3,
-    strokeDashArray: '5,5'
-  }]
 
 const LineChart = React.createClass({
-  transformData (data) {
-    let values = []
-    data.forEach((d) => {
-      if (d.closed !== '') {
-        let value = {
-          x: d.closed,
-          y: d
-        }
-      }
+  getInitialState () {
+    return {
+      lineData: [{
+        name: 'Customers over Time',
+        values: [{ x: 1436959681000, y: 11 },
+          { x: 1442122919000, y: 9 }],
+        strokeWidth: 3,
+        strokeDashArray: '5,5'
+      }]
+    }
+  },
+  componentWillMount () {
+    this.getLineChartData('metrics.json')
+  },
+  getLineChartData (url) {
+    let options = {
+      method: 'get'
+    }
+    var self = this
+
+    makeRequest(url, options)
+    .then((response) => {
+      const lineData = response
+      self.setState({lineData: lineData })
+    })
+    .catch((e) => {
+      console.log(e)
     })
   },
   render () {
@@ -39,7 +39,6 @@ const LineChart = React.createClass({
       <section>
         <LnChrt
           legend={true}
-          data={lineData}
           width='100%'
           height={400}
           viewBoxObject={{
@@ -50,7 +49,7 @@ const LineChart = React.createClass({
           }}
           yAxisLabel='Customers'
           xAxisLabel='Time'
-          data={lineData}
+          data={this.state.lineData}
           xAccessor={(d) => {
             return new Date(d.x)
           }
